16dd232e5d3287332ad32713bedcb1e5d91eb6af
Santiago Villarreal <santivilarley1010@gmail.com>
2023-04-12 10:48:51 -0600

    signup

--- proyecto-mopt/api/routes.js
+++ proyecto-mopt/api/routes.js
@@ -2,5 +2,5 @@ var controllerUsers = require('./controller.js');
 module.exports = function(app){
     app.put('/api/nuevoUsuario', controllerUsers.Guardar);
     app.post('/api/login', controllerUsers.Login);
-    app.post('/api/modificaPersona', controllerUser.Modificar); 
+    app.post('/api/modificaPersona', controllerUsers.Modificar); 
 }
